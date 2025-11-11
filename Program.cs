65fa035d61378892e1eb4268f43cd13f5b799da6
sover <kaparov-1337@mail.ru>
2025-11-11 14:27:16 +0300

    finally, cart and log work v2

--- Program.cs
+++ Program.cs
@@ -16,8 +16,7 @@ builder.Services.AddControllers();
 var jwt = builder.Configuration.GetSection("Jwt");
 var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["Key"]!));
 
-builder.Services
-    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(o =>
     {
         o.TokenValidationParameters = new()
@@ -28,32 +27,59 @@ builder.Services
             ValidateIssuerSigningKey = true,
             ValidIssuer = jwt["Issuer"],
             ValidAudience = jwt["Audience"],
-            IssuerSigningKey = key,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["Key"]!)),
             ClockSkew = TimeSpan.Zero
         };
+        o.IncludeErrorDetails = true;
+        o.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = ctx =>
+            {
+                Console.WriteLine("AUTH HDR: " + ctx.Request.Headers["Authorization"].ToString());
+                return Task.CompletedTask;
+            },
+            OnAuthenticationFailed = ctx =>
+            {
+                Console.WriteLine("JWT FAIL: " + ctx.Exception.Message);
+                return Task.CompletedTask;
+            }
+        };
     });
+
 builder.Services.AddAuthorization();
 
-// Swagger + кнопка Authorize
+// Swagger + кнопка Authorize   
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "ShopBTW API", Version = "v1" });
-    var scheme = new OpenApiSecurityScheme
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Name = "Authorization",
         Type = SecuritySchemeType.Http,
         Scheme = "bearer",
         BearerFormat = "JWT",
         In = ParameterLocation.Header,
-        Description = "Введите токен в формате: Bearer {token}"
-    };
-    c.AddSecurityDefinition("Bearer", scheme);
+        Description = "Укажите токен:"
+    });
+
     c.AddSecurityRequirement(new OpenApiSecurityRequirement
     {
-        { scheme, Array.Empty<string>() }
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id   = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
     });
 });
+
 
 var app = builder.Build();
 
